2051cda990f1f10aad041c01ebcf9096eb617867
Vinny Diehl <vinny.diehl@gmail.com>
2023-06-11 05:56:37 -0400

    Render grid lines statically

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,6 @@
 CELL_SIZE = 10
 LINE_COLOR = { r: 220, g: 220, b: 220 }.freeze
-GENERATION_TIME = 20
+GENERATION_TIME = 2
 
 class Cell
   attr_accessor :toggleable
@@ -68,8 +68,8 @@ class ConwaysGameOfLife
   def initialize(args)
     @args = args
     @solids = args.outputs.solids
-    @borders = args.outputs.borders
-    @lines = args.outputs.lines
+    @static_borders = args.outputs.static_borders
+    @static_lines = args.outputs.static_lines
     @mouse = args.inputs.mouse
     @kb = args.inputs.keyboard.key_down
 
@@ -83,6 +83,8 @@ class ConwaysGameOfLife
     @frames = 0
 
     @cells = Array.new(@grid_width) { |x| Array.new(@grid_height) { |y| Cell.new(x, y) } }
+
+    render_grid
   end
 
   def tick
@@ -119,24 +121,23 @@ class ConwaysGameOfLife
   end
 
   def render
-    render_grid
     render_cells
   end
 
   def render_grid
     # Outline
-    @borders << { x: 0, y: 0, w: @screen_width, h: @screen_height, **LINE_COLOR }
+    @static_borders << { x: 0, y: 0, w: @screen_width, h: @screen_height, **LINE_COLOR }
 
     # Verticals
     (1..@grid_width - 1).each do |x_i|
       x = x_i * CELL_SIZE
-      @lines << { x: x, x2: x, y: 0, y2: @screen_height, **LINE_COLOR }
+      @static_lines << { x: x, x2: x, y: 0, y2: @screen_height, **LINE_COLOR }
     end
 
     # Horizontals
     (1..@grid_height - 1).each do |y_i|
       y = y_i * CELL_SIZE
-      @lines << { x: 0, x2: @screen_width, y: y, y2: y, **LINE_COLOR }
+      @static_lines << { x: 0, x2: @screen_width, y: y, y2: y, **LINE_COLOR }
     end
   end
 
